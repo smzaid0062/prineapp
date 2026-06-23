0daea2151b2f295ad37a46a2a5c148ebc34004c6
Md Zaid <smzaid321@gmail.com>
2026-06-23 14:42:26 +0530

    first commit

---
+++ FirstDay.js
@@ -0,0 +1,58 @@
+// 30 Day JavaScript Chalange
+
+//Day 1
+
+
+//console.log("Thank you for giving me this oppurtunity. my name is sameer and i am form Bihar, i have completed my BCA Dgree form munger university munger, curruntly i am working with MERN Stack.")
+
+// JavaScript Variable :
+
+//  let my_name = "My Name";
+//  var my_names = "Is"
+//  const myy_name = "Zaid"
+
+//  console.log(my_name + my_names + myy_name )
+
+// JavaScript Has 8 Datatypes:
+// String
+// Numner
+// BigInt
+// Boolean
+// undefined
+// null
+// Symbol
+// Object (objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.)
+
+// Number
+// let length = 16
+// let weight = 7.8
+
+//String
+// let color = "yellow"
+// let lastName = "md zaid"
+
+// Boolean
+// let x = true
+// let y = false
+
+// Object
+// const My_Info = {
+//     Name : "md zaid",
+//     age : 19,
+//     Class : "BCA",
+//     Add : "Chakandra",
+// }
+// console.log(My_Info)
+
+
+//--------------------------------------------
+// JavaScripy Function
+
+1. 
+// function myFun(){
+//     let Name = "md zaid"
+//     let age = 19
+//     console.log(Name,age)
+// }
+
+// myFun()
